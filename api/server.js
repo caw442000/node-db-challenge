7906e58b0633c93b8efbf679648f91c010e7f429
cedric-winbush <cawinbushjr@gmail.com>
2020-03-20 10:19:18 -0500

    scaffold basic structure confirm helmet morgan are working build index.js and server.js test api is working --- it works

--- api/server.js
+++ api/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: "It is working"})
+})
 
 
 module.exports = server;
